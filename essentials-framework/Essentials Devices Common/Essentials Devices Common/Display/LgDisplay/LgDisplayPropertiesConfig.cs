bf71a32dc99991ada8475699f8a53d98efa696b0
Alex Johnson <alexj728@gmail.com>
2022-11-15 14:17:58 -0500

    Adds LG display control

---
+++ essentials-framework/Essentials Devices Common/Essentials Devices Common/Display/LgDisplay/LgDisplayPropertiesConfig.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Epi.Display.Lg
+{
+	public class LgDisplayPropertiesConfig
+	{
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("volumeUpperLimit")]
+        public int volumeUpperLimit { get; set; }
+
+        [JsonProperty("volumeLowerLimit")]
+        public int volumeLowerLimit { get; set; }
+
+        [JsonProperty("pollIntervalMs")]
+        public long pollIntervalMs { get; set; }
+
+        [JsonProperty("coolingTimeMs")]
+        public uint coolingTimeMs { get; set; }
+
+        [JsonProperty("warmingTimeMs")]
+        public uint warmingTimeMs { get; set; }
+
+        [JsonProperty("udpSocketKey")]
+        public string udpSocketKey { get; set; }
+
+        [JsonProperty("macAddress")]
+        public string macAddress { get; set; }
+
+        [JsonProperty("smallDisplay")]
+        public bool SmallDisplay { get; set; }
+	}
+}
