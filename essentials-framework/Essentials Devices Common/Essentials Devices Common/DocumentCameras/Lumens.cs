5fa79e39ff2a31b09009f93cc210ccff4219bbdd
Alex Johnson <alexj728@gmail.com>
2023-10-27 09:29:19 -0400

    Add controller for DmMd6xN type. Work on lumens doccam class.

--- essentials-framework/Essentials Devices Common/Essentials Devices Common/DocumentCameras/Lumens.cs
+++ essentials-framework/Essentials Devices Common/Essentials Devices Common/DocumentCameras/Lumens.cs
@@ -43,6 +43,88 @@ namespace PepperDash.Essentials.Devices.Common.Lumens
         /// <param name="e">Event args</param>
         private void Communication_BytesReceived(object sender, GenericCommMethodReceiveBytesArgs e)
         {
+            byte[] response = new byte[4];
+            uint? stxPos = null;
+            uint? etxPos = null;
+            for (uint i = 0; i < e.Bytes.Length; i++)
+            {
+                if (stxPos == null)
+                {
+                    if (e.Bytes[i] == 0xA0)
+                        stxPos = i;
+                }
+                else
+                {
+                    if (e.Bytes[i] == 0xAF)
+                    {
+                        etxPos = i;
+                        break;
+                    }
+                    else
+                    {
+                        uint responsePos = i-(uint)stxPos-1;
+                        if(responsePos > 3)
+                            break;
+                        response[responsePos] = e.Bytes[i];
+                    }
+                }
+            }
+            if (stxPos != null && etxPos != null && (etxPos - stxPos == 5))
+            {
+                processResponse(response);
+            }
+            else
+            {
+                Debug.Console(0, this, "Unable to find stx and etx in response");
+            }
+        }
+
+        private void processResponse(byte[] response)
+        {
+            if (response[3] != 0x00)
+            {
+                //NAK response
+                if (response[3] == 0x01)
+                {
+                    Debug.Console(0, this, "Command not executed");
+                }
+                //Unknown command response
+                else if (response[3] == 0x02)
+                {
+                    Debug.Console(0, this, "Unknown command");
+                }
+                else
+                {
+                    Debug.Console(0, this, "Unknown error in command");
+                }
+                return;
+            }
+
+            //Status response
+            if (response[0] == 0xB7)
+            {
+                if (response[2] == 0x00)
+                {
+                    PowerIsOn = false;
+                }
+                else if (response[2] == 0x01)
+                {
+                    PowerIsOn = true;
+                }
+            }
+
+            //Power response
+            else if (response[0] == 0xB1)
+            {
+                if (response[1] == 0x00)
+                {
+                    PowerIsOn = false;
+                }
+                else if (response[1] == 0x01)
+                {
+                    PowerIsOn = true;
+                }
+            }
         }
 
         #region IBridge Members
@@ -56,6 +138,8 @@ namespace PepperDash.Essentials.Devices.Common.Lumens
             }
 
             CommunicationMonitor.IsOnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
+            trilist.SetSigTrueAction(joinMap.PowerOn.JoinNumber, () => PowerOn());
+            trilist.SetSigTrueAction(joinMap.PowerOff.JoinNumber, () => PowerOff());
         }
 
         #endregion
@@ -64,11 +148,21 @@ namespace PepperDash.Essentials.Devices.Common.Lumens
         #region IPower Members
 
         public void PowerOn()
-        {          
+        {
+            var cmd = new byte[]
+			{
+				0xA0, 0xB1, 0x01, 0x00, 0x00, 0xAF
+			};
+            Communication.SendBytes(cmd);
         }
 
         public void PowerOff()
-        {         
+        {
+            var cmd = new byte[]
+			{
+				0xA0, 0xB1, 0x00, 0x00, 0x00, 0xAF
+			};
+            Communication.SendBytes(cmd);
         }
 
         public void PowerToggle()
@@ -85,13 +179,29 @@ namespace PepperDash.Essentials.Devices.Common.Lumens
 
         public BoolFeedback PowerIsOnFeedback { get; set; }
         bool _PowerIsOn;
+        public bool PowerIsOn
+        {
+            get
+            {
+                return _PowerIsOn;
+            }
+            set
+            {
+                _PowerIsOn = value;
+                PowerIsOnFeedback.FireUpdate();
+            }
+        }
 
         #endregion
 
         #region Poll
         public void Poll()
         {
-
+            var cmd = new byte[]
+			{
+				0xA0, 0xB7, 0x00, 0x00, 0x00, 0xAF
+			};
+            Communication.SendBytes(cmd);
         }
 
         #endregion
@@ -116,7 +226,7 @@ namespace PepperDash.Essentials.Devices.Common.Lumens
 
         [JoinName("PowerOn")]
         public JoinDataComplete PowerOn = new JoinDataComplete(new JoinData { JoinNumber = 1, JoinSpan = 1 },
-            new JoinMetadata { Description = "Power On", JoinCapabilities = eJoinCapabilities.FromSIMPL, JoinType = eJoinType.Digital });
+            new JoinMetadata { Description = "Power On", JoinCapabilities = eJoinCapabilities.ToFromSIMPL, JoinType = eJoinType.Digital });
 
         [JoinName("PowerOff")]
         public JoinDataComplete PowerOff = new JoinDataComplete(new JoinData { JoinNumber = 2, JoinSpan = 1 },
