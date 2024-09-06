f31f0157ce1c948960076cd622f659758cbb8edd
Alex Johnson <aj1@umd.edu>
2024-09-06 09:31:53 -0400

    Adds queueing for serial controlled lighting device

--- essentials-framework/Essentials Devices Common/Essentials Devices Common/Environment/Generic/SerialControlledLighting.cs
+++ essentials-framework/Essentials Devices Common/Essentials Devices Common/Environment/Generic/SerialControlledLighting.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
 using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.CrestronThread;
 using PepperDash.Core;
 using PepperDash.Essentials.Core;
 using PepperDash.Essentials.Core.Config;
@@ -19,12 +20,18 @@ namespace PepperDash.Essentials.Devices.Common.Environment.Generic
     {
         public IBasicCommunication Communication { get; private set; }
         public StatusMonitorBase CommunicationMonitor { get; private set; }
+        private CrestronQueue<string> _commandQueue;
+        private CMutex _commandMutex;
+        private CTimer _commandTimer;
+        private bool _queueWaiting = false;
+        private bool _commandReady = true;
+        string pollString;
 
         public SerialControlledLighting(string key, string name, IBasicCommunication comm, SerialControlledLightingPropertiesConfig props)
             : base(key, name)
         {
             Communication = comm;
-
+            Communication.TextReceived += Communication_TextReceived;
             if (props.Scenes != null)
             {
                 LightingScenes = props.Scenes;
@@ -32,8 +39,14 @@ namespace PepperDash.Essentials.Devices.Common.Environment.Generic
 
             if (props.PollString != null)
             {
-                CommunicationMonitor = new GenericCommunicationMonitor(this, Communication, 60000, 120000, 300000, props.PollString);
+                pollString = props.PollString;
+                CommunicationMonitor = new GenericCommunicationMonitor(this, Communication, 60000, 120000, 300000, Poll);
+                CommunicationMonitor.StatusChange += new EventHandler<MonitorStatusChangeEventArgs>(CommunicationMonitor_StatusChange);
             }
+
+            _commandQueue = new CrestronQueue<string>(20);
+            _commandMutex = new CMutex();
+            _commandTimer = new CTimer(commandTimeout, Timeout.Infinite);
         }
 
         public override bool CustomActivate()
@@ -51,6 +64,96 @@ namespace PepperDash.Essentials.Devices.Common.Environment.Generic
             CommunicationMonitor.IsOnlineFeedback.LinkInputSig(trilist.BooleanInput[joinMap.IsOnline.JoinNumber]);
         }
 
+        public void CommunicationMonitor_StatusChange(object o, MonitorStatusChangeEventArgs e)
+        {
+            if (e.Status == MonitorStatus.IsOk)
+            {
+            }
+            else
+            {
+                _commandQueue.Clear();
+            }
+        }
+
+        private void commandTimeout(object o)
+        {
+            Debug.Console(1, this, "Command timed out");
+            _commandReady = true;
+            ProcessQueue();
+        }
+
+        protected void readyForNextCommand()
+        {
+            _commandTimer.Stop(); //No need for timeout on last command
+            _commandReady = true;
+            ProcessQueue();
+        }
+
+        private void ProcessQueue()
+        {
+            CrestronInvoke.BeginInvoke((o) =>
+            {
+                //Thread safe queue processing below
+                if (_queueWaiting == false)
+                {
+                    _queueWaiting = true;
+                    if (_commandMutex.WaitForMutex())
+                    {
+                        Debug.Console(2, this, "Got command mutex");
+                        _queueWaiting = false;
+                        try
+                        {
+                            while (!_commandQueue.IsEmpty)
+                            {
+                                int count = 0;
+                                while (!_commandReady && (count < 50))
+                                {
+                                    Thread.Sleep(100);
+                                    count++;
+                                }
+                                string cmd = _commandQueue.TryToDequeue();
+                                _commandReady = false;
+                                _commandTimer.Reset(200); //Wait maximum 200 ms for response
+
+                                CrestronInvoke.BeginInvoke((obj) =>
+                                {
+                                    Communication.SendText(cmd);
+                                });
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.Console(0, this, "Exception processing command queue: {0}", ex);
+                        }
+                        finally
+                        {
+                            _commandMutex.ReleaseMutex();
+                            Debug.Console(2, this, "Released command mutex");
+                        }
+                    }
+                    else
+                    {
+                        _queueWaiting = false;
+                    }
+                }
+            });
+        }
+
+        public void QueueCommand(string cmd)
+        {
+            if (!_commandQueue.IsFull)
+            {
+                Debug.Console(2, this, "Queueing command: {0}", cmd);
+                _commandQueue.TryToEnqueue(cmd);
+                ProcessQueue();
+            }
+            else
+            {
+                Debug.Console(0, this, "Command queue is full! Dropping command.");
+                readyForNextCommand();
+            }
+        }
+
         /// <summary>
         /// Recalls the specified scene
         /// </summary>
@@ -61,6 +164,19 @@ namespace PepperDash.Essentials.Devices.Common.Environment.Generic
             if (LightingScenes != null && LightingScenes.Exists(o => o.ID == scene.ID))
             {
                 SelectScene((ushort)LightingScenes.FindIndex(o => o.ID == scene.ID));
+            }
+        }
+
+        /// <summary>
+        /// Communication bytes recieved
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e">Event args</param>
+        private void Communication_TextReceived(object sender, GenericCommMethodReceiveTextArgs e)
+        {
+            if(e.Text.Contains("\r\n"))
+            {
+                readyForNextCommand();
             }
         }
 
@@ -78,15 +194,19 @@ namespace PepperDash.Essentials.Devices.Common.Environment.Generic
                     Debug.Console(1, this, "Selecting Scene: '{0}'", LightingScenes[scene].ID);
                     if (LightingScenes[scene].Command != null)
                     {
-                        Communication.SendText(LightingScenes[scene].Command);
+                        QueueCommand(LightingScenes[scene].Command);
                     }
-                    CrestronEnvironment.Sleep(1000);
                     if (LightingScenes[scene].Command2 != null)
                     {
-                        Communication.SendText(LightingScenes[scene].Command2);
+                        QueueCommand(LightingScenes[scene].Command2);
                     }
                 }
             }
+        }
+
+        public void Poll()
+        {
+            QueueCommand(pollString);
         }
     }
 
